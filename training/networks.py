09cc8c25e035dcc4b4669843fd9d835166fd7f27
1Nuyoch <151995927+1Nuyoch@users.noreply.github.com>
2025-07-28 19:19:02 +0800

    networks.py

--- training/networks.py
+++ training/networks.py
@@ -258,35 +258,7 @@ class SynthesisNetwork(torch.nn.Module):
         return img
 
 
-'''
-class QuickGELU(nn.Module):   # 3++++
-    def forward(self, x: torch.Tensor):
-        return x * torch.sigmoid(1.702 * x)
 
-from collections import OrderedDict
-class ResidualAttentionBlock(nn.Module): # 3++++ attn
-    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
-        super().__init__()
-
-        self.attn = nn.MultiheadAttention(d_model, n_head)
-        self.ln_1 = nn.LayerNorm(d_model)
-        self.mlp = nn.Sequential(OrderedDict([
-            ("c_fc", nn.Linear(d_model, d_model * 4)),
-            ("gelu", QuickGELU()),
-            ("c_proj", nn.Linear(d_model * 4, d_model))
-        ]))
-        self.ln_2 = nn.LayerNorm(d_model)
-        self.attn_mask = attn_mask
-
-    def attention(self, x: torch.Tensor):
-        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
-        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
-
-    def forward(self, x: torch.Tensor):
-        x = x + self.attention(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))     # ///////
-        return x
-'''
 class QuickGELU(nn.Module):
     def forward(self, x: torch.Tensor):
         return x * torch.sigmoid(1.702 * x)
